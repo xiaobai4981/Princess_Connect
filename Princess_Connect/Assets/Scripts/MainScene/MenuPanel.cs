d0e81654a1ae68e63bbfda6f442a0a5953e70511
xiaobai <907242459@qq.com>
2025-04-18 20:14:45 +0800

    菜单页面返回标题页面功能加入

--- Princess_Connect/Assets/Scripts/MainScene/MenuPanel.cs
+++ Princess_Connect/Assets/Scripts/MainScene/MenuPanel.cs
@@ -16,9 +16,44 @@ public class MenuPanel : BasePanel
         
     }
 
+    // ÷ÿ–¥Click
+    protected override void ClickBtn(string btnName)
+    {
+        switch (btnName)
+        {
+            case "Item":
+                
+                break;
+            case "Setting":
+                
+                break;
+            case "Designation":
+                
+                break;
+            case "BackToStart":
+                BackToStart();
+                break;
+        }
+    }
+
     public override void UpdatePlayerName(string nowPlayerName)
     {
         this.nowPlayerName = nowPlayerName;
+    }
+
+    private void BackToStart()
+    {
+        DatabaseMgr.Instance.DatabaseClose();
+        UIMgr.Instance.ShowPanel<StartPanel>(E_UILayer.Bottom, (panel) =>
+        {
+
+        });
+        UIMgr.Instance.HidePanel<MenuPanel>(true);
+        UIMgr.Instance.HidePanel<LotteryPanel>(true);
+        UIMgr.Instance.HidePanel<BeginPanel>(true);
+        UIMgr.Instance.HidePanel<CharacterPanel>(true);
+        UIMgr.Instance.HidePanel<ADVPanel>(true);
+        UIMgr.Instance.HidePanel<BottomBtnPanel>(true);
     }
 
     public override void HideMe()
@@ -30,4 +65,5 @@ public class MenuPanel : BasePanel
     {
 
     }
+
 }
